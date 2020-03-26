b29e4380ecbe60fa161ef658112ee0b228633171
Uppalapati, Prithvi Raju (US - Hyderabad) <prituppalapati@deloitte.com>
2020-03-26 15:02:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser')
+var httpsRedirect = require("express-https-redirect");
+
+app.use("/", httpsRedirect());
+app.use(express.static(path.join(__dirname, './public')));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+
+/// app runs in port
+app.listen(process.env.PORT || 3000, function () {
+  console.log('listening at 3000');
+})
